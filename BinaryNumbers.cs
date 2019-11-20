8f423ef0222e428324d6b2a104e08cf232f93e30
Oliver <ocben1@gmail.com>
2019-11-21 08:40:53 +1100

    day 10

--- BinaryNumbers.cs
+++ BinaryNumbers.cs
@@ -12,37 +12,39 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
+class Solution
+{
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         int n = Convert.ToInt32(Console.ReadLine());
-            int i = 0, k=0;
-            int count = 0; 
-            // array contain the binary 
-            int[] a= new int[100] ;
-            while (n > 0)
-            {
-                a[i] = n % 2;
-                n = n / 2;
-                i++;
-            }
-         
-            for (int j=0; j<a.Length; j++)
-            {
-                if (a[j] == 1)
-                {
-                    count += 1;
-                }
-                else
-                {
-                    if(count > k)
-                    {
-                        k = count;
-                    }
-                    count = 0;
-                }
+        int i = 0, k = 0;
+        int count = 0;
+        // array contain the binary 
+        int[] a = new int[100];
+        while (n > 0)
+        {
+            a[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
 
+        for (int j = 0; j < a.Length; j++)
+        {
+            if (a[j] == 1)
+            {
+                count += 1;
             }
+            else
+            {
+                if (count > k)
+                {
+                    k = count;
+                }
+                count = 0;
+            }
+
+        }
         Console.WriteLine(k);
     }
 }
