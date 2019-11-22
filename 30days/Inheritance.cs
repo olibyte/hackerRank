079a873e9b6b487467eb81517bf833ae73b68c37
Oliver <ocben1@gmail.com>
2019-11-22 18:21:04 +1100

    day 13

--- 30days/Inheritance.cs
+++ 30days/Inheritance.cs
@@ -1,50 +1,48 @@
 using System;
 using System.Linq;
 
-class Person
-{
-    protected string firstName;
-    protected string lastName;
-    protected int id;
-
-    public Person() { }
-    public Person(string firstName, string lastName, int identification)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.id = identification;
-    }
-    public void printPerson()
-    {
-        Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
-    }
+class Person{
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+	
+	public Person(){}
+	public Person(string firstName, string lastName, int identification){
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.id = identification;
+	}
+	public void printPerson(){
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+	}
 }
 
-class Student : Person
-{
-    private int[] testScores;
+class Student : Person{
+    private int[] testScores;  
     private int total = 0;
     private int average = 0;
     private char grade;
-
-    //        /*	
-    //        *   Class Constructor
-    //        *   
-    //        *   Parameters: 
-    //        *   firstName - A string denoting the Person's first name.
-    //        *   lastName - A string denoting the Person's last name.
-    //        *   id - An integer denoting the Person's ID number.
-    //        *   scores - An array of integers denoting the Person's test scores.
-    //        */
+    /*	
+    *   Class Constructor
+    *   
+    *   Parameters: 
+    *   firstName - A string denoting the Person's first name.
+    *   lastName - A string denoting the Person's last name.
+    *   id - An integer denoting the Person's ID number.
+    *   scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
     public Student(string firstName, string lastName, int id, int[] scores)
                : base(firstName, lastName, id)
     {
 
         this.testScores = scores;
     }
-    //    /*	
-    //    *   Method Name: Calculate
-    //    *   Return: A character denoting the grade.
+    /*	
+    *   Method Name: Calculate
+    *   Return: A character denoting the grade.
+    */
+    // Write your method here
     public char Calculate()
     {
         int divide = testScores.Length;
@@ -53,7 +51,6 @@ class Student : Person
             total += testScores[i];
         }
         average = total / divide;
-
         if (average >= 90 && average <= 100)
             grade = 'O';
         if (average >= 80 && average < 90)
@@ -66,29 +63,25 @@ class Student : Person
             grade = 'D';
         if (average < 40)
             grade = 'T';
-
         return grade;
     }
 }
 
-class Solution
-{
-    static void Main()
-    {
-        string[] inputs = Console.ReadLine().Split();
-        string firstName = inputs[0];
-        string lastName = inputs[1];
-        int id = Convert.ToInt32(inputs[2]);
-        int numScores = Convert.ToInt32(Console.ReadLine());
-        inputs = Console.ReadLine().Split();
-        int[] scores = new int[numScores];
-        for (int i = 0; i < numScores; i++)
-        {
-            scores[i] = Convert.ToInt32(inputs[i]);
-        }
-
-        Student s = new Student(firstName, lastName, id, scores);
-        s.printPerson();
-        Console.WriteLine("Grade: " + s.Calculate() + "\n");
-    }
+class Solution {
+	static void Main() {
+		string[] inputs = Console.ReadLine().Split();
+		string firstName = inputs[0];
+	  	string lastName = inputs[1];
+		int id = Convert.ToInt32(inputs[2]);
+		int numScores = Convert.ToInt32(Console.ReadLine());
+		inputs = Console.ReadLine().Split();
+	  	int[] scores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			scores[i]= Convert.ToInt32(inputs[i]);
+		} 
+	  	
+		Student s = new Student(firstName, lastName, id, scores);
+		s.printPerson();
+		Console.WriteLine("Grade: " + s.Calculate() + "\n");
+	}
 }
