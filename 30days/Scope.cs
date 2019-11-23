d4e2849a4d9ffc9c06be17bde7b8a009506fbe4d
Oliver <ocben1@gmail.com>
2019-11-23 11:25:01 +1100

    day 14

---
+++ 30days/Scope.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+	// Add your code here
+    public Difference(int[] elements)
+    {
+        this.elements = elements;
+    }
+
+    public void computeDifference()
+    {
+        int max = 0;
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            for (int j = 0; j < elements.Length; j++)
+            {
+                int abs = Math.Abs(elements[i] - elements[j]);
+                if (abs > max) max = abs;
+            }
+        }
+
+        maximumDifference = max;
+    }
+} // End of Difference Class
+
+class Solution {
+    static void Main(string[] args) {
+        Convert.ToInt32(Console.ReadLine());
+        
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        
+        Difference d = new Difference(a);
+        
+        d.computeDifference();
+        
+        Console.Write(d.maximumDifference);
+    }
+}
