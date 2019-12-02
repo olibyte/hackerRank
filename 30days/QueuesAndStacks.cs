99cc3b1d3421d2460857acbcd32d4b0ab8e59270
Oliver <ocben1@gmail.com>
2019-12-03 08:22:33 +1100

    day 18

--- 30days/QueuesAndStacks.cs
+++ 30days/QueuesAndStacks.cs
@@ -2,40 +2,71 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Solution {
+class Solution
+{
     //Write your code here
+    public Queue<char> q;
+    public Stack<char> s;
 
+    public Solution()
+    {
+        q = new Queue<char>();
+        s = new Stack<char>();
+    }
+    public void pushCharacter(char ch)
+    {
+        s.Push(ch);
+    }
+    public void enqueueCharacter(char ch)
+    {
+        q.Enqueue(ch);
+    }
+    public char popCharacter()
+    {
+        return s.Pop();
+    }
+    public char dequeueCharacter()
+    {
+        return q.Dequeue();
+    }
 
-    static void Main(String[] args) {
+    static void Main(String[] args)
+    {
         // read the string s.
         string s = Console.ReadLine();
-        
+
         // create the Solution class object p.
         Solution obj = new Solution();
-        
+
         // push/enqueue all the characters of string s to stack.
-        foreach (char c in s) {
+        foreach (char c in s)
+        {
             obj.pushCharacter(c);
             obj.enqueueCharacter(c);
         }
-        
+
         bool isPalindrome = true;
-        
+
         // pop the top character from stack.
         // dequeue the first character from queue.
         // compare both the characters.
-        for (int i = 0; i < s.Length / 2; i++) {
-            if (obj.popCharacter() != obj.dequeueCharacter()) {
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (obj.popCharacter() != obj.dequeueCharacter())
+            {
                 isPalindrome = false;
-                
+
                 break;
             }
         }
-        
+
         // finally print whether string s is palindrome or not.
-        if (isPalindrome) {
+        if (isPalindrome)
+        {
             Console.Write("The word, {0}, is a palindrome.", s);
-        } else {
+        }
+        else
+        {
             Console.Write("The word, {0}, is not a palindrome.", s);
         }
     }
