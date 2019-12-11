57899aceb895498dc661f26601c7371348e47e0b
Oliver <ocben1@gmail.com>
2019-12-11 17:42:52 +1100

    day 26

---
+++ 30days/NestedLogic.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        var actually = Console.ReadLine().Split(' ');
+        var da = int.Parse(actually[0]);
+        var ma = int.Parse(actually[1]);
+        var ya = int.Parse(actually[2]);
+
+        var expected = Console.ReadLine().Split(' ');
+        var de = int.Parse(expected[0]);
+        var me = int.Parse(expected[1]);
+        var ye = int.Parse(expected[2]);
+
+        var fine = 0;
+
+        if (ya > ye) fine = 10000;
+        else if (ya == ye)
+        {
+            if (ma > me) fine = (ma - me) * 500;
+            else if (ma == me && da > de) fine = (da - de) * 15;
+        }
+
+        Console.WriteLine(fine);
+    }
+}
+
