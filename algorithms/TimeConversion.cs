6d82356479f77874fe1bc7c5f555bf0cb702b07d
Oliver <ocben1@gmail.com>
2019-12-17 12:29:45 +1100

    time conversion

---
+++ algorithms/TimeConversion.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        /*
+         * Write your code here.
+         */
+        var dtHour = Convert.ToDateTime(s);
+        string militaryTime = dtHour.ToString("HH:mm:ss");
+
+        return militaryTime;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
