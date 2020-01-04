e2fdc0838fafe0911675d53a4880247c975090e0
Oliver <ocben1@gmail.com>
2020-01-05 08:59:21 +1100

    add breaking the records

---
+++ algorithms/BreakingTheRecords.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp))
+        ;
+        int[] result = breakingRecords(scores);
+
+        textWriter.WriteLine(string.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
