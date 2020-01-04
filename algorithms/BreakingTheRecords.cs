b00d822f6571c7ba5da14bee0023ed0ba6b409c0
Oliver <ocben1@gmail.com>
2020-01-05 09:02:14 +1100

    Breaking the records

--- algorithms/BreakingTheRecords.cs
+++ algorithms/BreakingTheRecords.cs
@@ -12,15 +12,35 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
+class Solution
+{
 
     // Complete the breakingRecords function below.
-    static int[] breakingRecords(int[] scores) {
+    static int[] breakingRecords(int[] scores)
+    {
+        int min = scores[0];
+        int max = scores[0];
+        int[] records = new int[2];
+        for (int i = 1; i < scores.Length; i++)
+        {
+            if (scores[i] > max)
+            {
+                max = scores[i];
+                records[0]++;
 
+            }
+            else if (scores[i] < min)
+            {
+                min = scores[i];
+                records[1]++;
+            }
+        }
+        return records;
 
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         int n = Convert.ToInt32(Console.ReadLine());
