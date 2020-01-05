440ee76b1aaf05ca1689aeb252e42256aa635853
Oliver <ocben1@gmail.com>
2020-01-06 05:31:15 +1100

    Between Two Sets

---
+++ algorithms/BetweenTwoSets.cs
@@ -0,0 +1,74 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        var aFirstElement = a.First();
+        var aLastElement = a.Last();
+        var bFirstElement = b.First();
+        var count = 0;
+
+        var elementsBetween = new List<int>();
+
+        for (int i = aLastElement; i <= bFirstElement; i++)
+        {
+            if (a.TrueForAll(x => Mod(i, x)) && b.TrueForAll(x => Mod(i, x)))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    private static bool Mod(int i, int j) => i > j ? i % j == 0 : j % i == 0;
+
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        textWriter.WriteLine(total);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
