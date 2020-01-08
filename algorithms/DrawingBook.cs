0cb894a089433648c27a7be5093f435bf56e58cb
Oliver <ocben1@gmail.com>
2020-01-08 19:11:51 +1100

    Drawing Book

---
+++ algorithms/DrawingBook.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p)
+    {
+        /*
+         * Write your code here.
+         */
+
+
+        int flipFromFront = p / 2;
+        int flipFromBack = ((n / 2) - (p / 2));
+
+        if (flipFromFront < flipFromBack) return flipFromFront;
+        else return flipFromBack;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        int result = pageCount(n, p);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
