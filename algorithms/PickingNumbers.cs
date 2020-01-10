9f776b37425e6fd9005d1ea6fb095ad802d3263a
Oliver <ocben1@gmail.com>
2020-01-11 01:28:04 +1100

    picking numbers

---
+++ algorithms/PickingNumbers.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+        int result = 0;
+        //declare integer array that orders each item.
+        int[] counter = new int[100];
+
+        foreach (var item in a)
+        {
+            counter[item] += 1;
+        }
+        for (int i = 1; i < counter.Length; i++)
+        {
+            int curCounter = counter[i] + counter[i - 1];
+            if (curCounter > result) { result = curCounter; }
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
