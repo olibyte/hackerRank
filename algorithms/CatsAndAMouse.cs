b0e7acd09b29888c3e6a373067693fb021dae67f
Oliver <ocben1@gmail.com>
2020-01-10 18:03:30 +1100

    reformat cats and a mouse

--- algorithms/CatsAndAMouse.cs
+++ algorithms/CatsAndAMouse.cs
@@ -12,27 +12,30 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
+class Solution
+{
 
     // Complete the catAndMouse function below.
-    static string catAndMouse(int x, int y, int z) {
+    static string catAndMouse(int x, int y, int z)
+    {
         //distance betweens cats and mouse
-        int catA = Math.Abs(x-z);
-        int catB = Math.Abs(y-z);
+        int catA = Math.Abs(x - z);
+        int catB = Math.Abs(y - z);
 
-        if(catA < catB) return "Cat A";
-        else if(catA > catB) return "Cat B";
-        else if (catA == catB) return "Mouse C";
-        return "";
+        if (catA < catB) return "Cat A";
+        else if (catA > catB) return "Cat B";
+        return "Mouse C";
 
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         int q = Convert.ToInt32(Console.ReadLine());
 
-        for (int qItr = 0; qItr < q; qItr++) {
+        for (int qItr = 0; qItr < q; qItr++)
+        {
             string[] xyz = Console.ReadLine().Split(' ');
 
             int x = Convert.ToInt32(xyz[0]);
