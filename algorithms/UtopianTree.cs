09de5fe8a9b8f703ae26382f4b141b86a9b5b3c3
Oliver <ocben1@gmail.com>
2020-01-13 08:09:19 +1100

    utopian tree

---
+++ algorithms/UtopianTree.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) {
+        if(n == 0)
+        {
+            return 1;
+        }
+        else if(n % 2 == 1)
+        {
+            return 2 * (utopianTree(n-1));
+        }
+        else
+        {
+            return 1 + (utopianTree(n-1));
+        }
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = utopianTree(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
