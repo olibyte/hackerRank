d4dd1d011ddd87e184997a5564852d6f1dfe1ce7
Oliver <ocben1@gmail.com>
2020-01-26 13:59:49 +1100

    extra long factorials

---
+++ algorithms/ExtraLongFactorials.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger factorial = new BigInteger(1);
+
+        for(int i = 1; i < n; i++)
+        {
+            factorial += factorial*i;
+        }
+        Console.WriteLine(factorial);
+
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        extraLongFactorials(n);
+    }
+}
