7763212694b9629664b16ddcdc702b5743a75034
Oliver <ocben1@gmail.com>
2020-01-29 08:17:43 +1100

    minimum absolute difference

---
+++ interviewPreparationKit/greedyAlgorithms/MinimumAbsoluteDifference.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumAbsoluteDifference function below.
+    static int minimumAbsoluteDifference(int[] arr) {
+        Array.Sort(arr);
+
+        var min = Math.Abs(arr[0] - arr[1]);
+
+        for(int i = 1; i < arr.Length-1; i++)
+        {
+            var abs = Math.Abs(arr[i] - arr[i+1]);
+
+            if (abs < min)
+            {
+                min = abs;
+            }
+        }
+        return min;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int result = minimumAbsoluteDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
