1d21f1056a8265ad44d7845735e3e99b18b96542
Oliver <ocben1@gmail.com>
2020-02-11 08:13:25 +1100

    comparator

---
+++ interviewPreparationKit/sorting/Comparator.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+    int result = a.score - b.score;
+        if (result == 0) {
+            result = a.name.compareTo(b.name); //string comparator
+            return result < 0 ? -1 : 1;
+        }
+        return result < 0 ? 1 : -1;
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
