8e153f47842b07d869cee8e63e5e781f45c5fe99
Oliver <ocben1@gmail.com>
2020-02-12 08:17:02 +1100

    add simpler solution to sherlock and the valid string

--- interviewPreparationKit/stringManipulation/SherlockAndTheValidString.cs
+++ interviewPreparationKit/stringManipulation/SherlockAndTheValidString.cs
@@ -60,6 +60,52 @@ class Solution
         return isValid ? "YES" : "NO";
 
     }
+    /*--------------simpler solution-----------*/
+    //     static string isValid(string s) {
+    //   var dict = new Dictionary<char, int>();
+
+    //     foreach(var c in s)
+    //     {
+    //         if(dict.ContainsKey(c))
+    //         {
+    //             dict[c] += 1;
+    //         }
+    //         else
+    //         {
+    //             dict.Add(c, 1);
+    //         }
+    //     }
+
+
+
+    //     int gap = 0;
+    //     int common = 0;
+    //     foreach(var key in dict.Keys)
+    //     {
+    //         if(common == 0)
+    //         {
+    //             common = dict[key];
+    //         }
+    //         else{
+    //             if(Math.Abs(dict[key] - common) > 1 && dict[key] != 1)
+    //             {
+    //                 return "NO";
+    //             }
+    //             else if(Math.Abs(dict[key] - common) == 1 || dict[key] == 1)
+    //             {
+    //                 gap++;
+    //                 if(gap > 1)
+    //                 {
+    //                     return "NO";
+    //                 }
+    //             }
+    //         }
+
+    //     }
+
+    //     return "YES";
+
+    // }
 
     static void Main(string[] args)
     {
