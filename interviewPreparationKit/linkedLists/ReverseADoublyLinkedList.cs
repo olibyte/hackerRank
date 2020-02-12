e5ab923fde16ea243fd97c5669d1f74b44d17435
Oliver <ocben1@gmail.com>
2020-02-13 09:07:04 +1100

    reverse a doubly linked list

---
+++ interviewPreparationKit/linkedLists/ReverseADoublyLinkedList.cs
@@ -0,0 +1,121 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintDoublyLinkedList(DoublyLinkedListNode node, string sep, TextWriter textWriter) {
+        while (node != null) {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+
+ var currentNode = head;
+
+        DoublyLinkedListNode prevNode = null;
+        DoublyLinkedListNode nextNode = null;
+
+        while(currentNode != null)
+        {
+            nextNode = currentNode.next;
+
+            var temp = currentNode.next;
+            currentNode.next = currentNode.prev;
+            currentNode.prev = temp;
+
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        return prevNode;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            DoublyLinkedListNode llist1 = reverse(llist.head);
+
+            PrintDoublyLinkedList(llist1, " ", textWriter);
+            textWriter.WriteLine();
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
