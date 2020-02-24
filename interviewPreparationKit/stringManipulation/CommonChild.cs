75e2821623d6d162c42c48545d7001f9fc6486ab
Oliver <ocben1@gmail.com>
2020-02-25 08:15:28 +1100

    common child

---
+++ interviewPreparationKit/stringManipulation/CommonChild.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the commonChild function below.
+    static int commonChild(string s1, string s2) {
+ int[,] c = new int[s1.Length + 1, s2.Length + 1];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    if (s1[i] == s2[j])
+                        c[i + 1, j + 1] = c[i, j] + 1;
+                    else
+                        c[i + 1, j + 1] = Math.Max(c[i + 1, j], c[i, j + 1]);
+                }
+            }
+            return c[s1.Length, s2.Length];
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = commonChild(s1, s2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
