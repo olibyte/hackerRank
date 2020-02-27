09ef878ca4996caa661f5316bf7f78f53f82026d
Oliver <ocben1@gmail.com>
2020-02-28 08:38:29 +1100

    pairs

---
+++ interviewPreparationKit/search/Pairs.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr)
+    {
+        int pairsCount = 0, difference = 0;
+
+        Array.Sort(arr);
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                difference = arr[i] - arr[j];
+
+                if (difference == k)
+                {
+                    pairsCount++;
+                }
+                else if (difference > k)
+                {
+                    break;
+                }
+            }
+        }
+
+        return pairsCount;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int result = pairs(k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
