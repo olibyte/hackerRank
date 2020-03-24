f1ddab5c14d998cbe8ded408bf0c498a0969b887
Oliver <ocben1@gmail.com>
2020-03-24 14:10:18 +1100

    maximum XOR

--- interviewPreparationKit/miscellaneous/MaximumXOR.cs
+++ interviewPreparationKit/miscellaneous/MaximumXOR.cs
@@ -15,22 +15,99 @@ using System;
 class Solution {
 
     // Complete the maxXor function below. TO DO: Optimise the solution to execute within the time limits.
-    static int[] maxXor(int[] arr, int[] queries) {
-        // solve here
-        int[] maxXor= new int [queries.Length];
-        Array.Sort(arr);
+
+    //The naive approach would be to traverse the array for every query and find the maximum XOR - O(n*q)
+    // static int[] maxXor(int[] arr, int[] queries) {
+    //     // solve here
+    //     int[] maxXor= new int [queries.Length];
+    //     Array.Sort(arr);
        
-        for(int i=0; i<queries.Length; i++){
-            int max = 0;
-           for(int j=0; j<arr.Length; j++){
-               int xor = (queries[i]^arr[j]); 
-               if(xor>max){
-                 max=xor;
-               }  
-           }
-           maxXor[i]=max;
+    //     for(int i=0; i<queries.Length; i++){
+    //         int max = 0;
+    //        for(int j=0; j<arr.Length; j++){
+    //            int xor = (queries[i]^arr[j]); 
+    //            if(xor>max){
+    //              max=xor;
+    //            }  
+    //        }
+    //        maxXor[i]=max;
+    //     }
+    //     return maxXor;
+    // }
+
+    //We can solve this problem at O(q*log(max(Ai))) using a trie data structure. The idea is to store every element in its binary form in a trie.
+    //Now, for each query let the number be x, then we traverse bit by bit through the number along the nodes of the trie with maximizing the xor(This will take  complexity.
+
+
+    class Trie
+    {
+        Trie left;
+        Trie right;
+
+        public void Insert(Trie head, int n)
+        {
+            for (int i = 31; i >= 0; i--)
+            {
+                int value = (n >> i) & 1;
+                if (value == 0)
+                {//move left
+                    if (head.left == null)
+                        head.left = new Trie();
+                    head = head.left;
+                }
+                else
+                {//move right
+                    if (head.right == null)
+                        head.right = new Trie();
+                    head = head.right;
+                }
+            }
         }
-        return maxXor;
+
+        public uint MaxXor(Trie head, int n)
+        {
+            uint max = 0;
+            for (int i = 31; i >= 0; i--)
+            {
+                int value = (n >> i) & 1;
+                //for max xor you have to move alternate
+                if (value == 0)
+                {//move right
+                    if (head.right != null)
+                    {
+                        max += (uint)Math.Pow(2, i);
+                        head = head.right;
+                    }
+                    else
+                        head = head.left;
+                }
+                else
+                {//move left
+                    if (head.left != null)
+                    {
+                        max += (uint)Math.Pow(2, i);
+                        head = head.left;
+                    }
+                    else
+                        head = head.right;
+                }
+            }
+            return max;
+        }
+    }
+    static int[] maxXor(int[] arr, int[] queries)
+    {
+        int[] result = new int[queries.Length];
+        Trie head = new Trie();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            head.Insert(head, arr[i]);
+        }
+        for (int i = 0; i < queries.Length; i++)
+        {
+            result[i] = Convert.ToInt32(head.MaxXor(head, queries[i]));
+        }
+        return result;
     }
 
     static void Main(string[] args) {
