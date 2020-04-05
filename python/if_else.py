7a44ea92bda86f36c4eb55c74dca6015c537b3f5
Oliver <ocben1@gmail.com>
2020-04-06 07:46:25 +1000

    if else

--- python/if_else.py
+++ python/if_else.py
@@ -12,14 +12,16 @@ import random
 import re
 import sys
 
-if n % 2 == 1:
-    print('Weird')
-# elif n % 2 == 0:
-if n >= 2 and n <= 5:
-    print ('Not Weird')
-elif n >= 6 and n <= 20:
-    print('Weird')
-else: 
-    print('Not Weird')
+
 if __name__ == '__main__':
     n = int(input().strip())
+
+    if n % 2 == 1:
+        print('Weird')
+    # elif n % 2 == 0:
+    elif n >= 2 and n <= 5:
+        print ('Not Weird')
+    elif n >= 6 and n <= 20:
+        print('Weird')
+    else: 
+        print('Not Weird')
