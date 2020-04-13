e4e964fbb73573855bbc733f877dc080a24e6a4a
Oliver <ocben1@gmail.com>
2020-04-13 12:06:51 +1000

    chocolate feast

---
+++ algorithms/chocolate_feast.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the chocolateFeast function below.
+def chocolateFeast(n, c, m):
+    result = 0
+    bars = int(n/c)
+    result += bars
+    wrappers = bars
+    while wrappers >= m:
+        bars = int(wrappers / m)
+        wrappers = wrappers % m
+        wrappers += bars
+        result += bars
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        ncm = input().split()
+
+        n = int(ncm[0])
+
+        c = int(ncm[1])
+
+        m = int(ncm[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
