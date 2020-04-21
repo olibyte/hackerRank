8be73650ae862945c16ac57a617246ee37bd5d3c
Oliver <ocben1@gmail.com>
2020-04-22 07:48:51 +1000

    flatland space station

---
+++ algorithms/flatland_space_station.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flatlandSpaceStations function below.
+def flatlandSpaceStations(n, c):
+    c.sort()
+    r = list()
+    r.append(c[0])
+    r.append(n-1-c[len(c)-1])
+    for i in range(1,len(c)):
+        r.append((c[i]-c[i-1])//2)
+
+    return max(r)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = flatlandSpaceStations(n, c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
