45706d4a1acd303cd1df5e1a46a67caa19c74d15
Oliver <ocben1@gmail.com>
2020-04-25 08:47:32 +1000

    sock merchant

--- interviewPreparationKit/warmup/sock_merchant.py
+++ interviewPreparationKit/warmup/sock_merchant.py
@@ -8,14 +8,14 @@ import sys
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    candidates = []
+    candidates = [] #list to keep track of sock
     pairs = 0
     for sock in ar:
-        if sock in candidates:
-            candidates.remove(sock)
+        if sock in candidates: #if we already have a sock in our candidates list
+            candidates.remove(sock) #remove it and increase the count of pairs
             pairs += 1
         else:
-            candidates.append(sock)
+            candidates.append(sock) #add it to our list
 
     return pairs
 if __name__ == '__main__':
