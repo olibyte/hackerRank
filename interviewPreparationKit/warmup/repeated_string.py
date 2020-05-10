eb69c5edcf143349c3a2297f61711b03d0ca6bc6
Oliver <ocben1@gmail.com>
2020-05-11 07:56:29 +1000

    to do repeatedString

--- interviewPreparationKit/warmup/repeated_string.py
+++ interviewPreparationKit/warmup/repeated_string.py
@@ -8,7 +8,7 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
-
+    return True
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
