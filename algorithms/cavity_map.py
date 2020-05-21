2edd45ad1ae28ca5ecf7eabf287e9de917bf8795
Oliver <ocben1@gmail.com>
2020-05-22 08:46:46 +1000

    to do cavity map

---
+++ algorithms/cavity_map.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the cavityMap function below.
+def cavityMap(grid):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    grid = []
+
+    for _ in range(n):
+        grid_item = input()
+        grid.append(grid_item)
+
+    result = cavityMap(grid)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
