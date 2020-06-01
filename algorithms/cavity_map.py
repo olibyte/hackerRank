4a47f0234a15a8b623d3f7e23304ed71eeb72de3
Oliver <ocben1@gmail.com>
2020-06-02 07:08:41 +1000

    cavity map

--- algorithms/cavity_map.py
+++ algorithms/cavity_map.py
@@ -8,6 +8,17 @@ import sys
 
 # Complete the cavityMap function below.
 def cavityMap(grid):
+    grid = list(map(list, grid))
+    for i in range(1, len(grid) -1): #i --> top, bottom
+        for j in range(1, len(grid)-1): #j --> left,right
+            if \
+            grid[i-1][j] != 'X' and int(grid[i-1][j]) < int(grid[i][j]) and \
+            grid[i+1][j] != 'X' and int(grid[i+1][j]) < int(grid[i][j]) and \
+            grid[i][j-1] != 'X' and int(grid[i][j-1]) < int(grid[i][j]) and \
+            grid[i][j+1] != 'X' and int(grid[i][j+1]) < int(grid[i][j]):
+                grid[i][j] = 'X'
+
+    return list(map(''.join, grid))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
