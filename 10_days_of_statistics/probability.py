165772c50a9465dfa651ff245bdf29339682a0d6
Oliver <ocben1@gmail.com>
2020-06-07 00:32:36 +1000

    solved

--- 10_days_of_statistics/probability.py
+++ 10_days_of_statistics/probability.py
@@ -33,7 +33,31 @@ D2 = [1,2,3,4,5,6]
 o, t = get_outcomes_totals(D1,D2)
 p = get_prob(t,9)
 p2 = get_prob_more_dice(o,t,6)
-print(o)
-print(t)
-print(p)
-print(p2)
+# print(o)
+# print(t)
+# print(p)
+# print(p2)
+
+x = {"red":4,"black":3}
+y = {"red":5,"black":4}
+z = {"red":4,"black":4}
+#prob that 2 are red and 1 is black
+print((2/7)*(2/9)*(1/8))
+# rrb
+# rbr
+# brr
+
+# rrr
+# rbr
+
+# rrb/
+# rbb
+
+# bbb
+# brr
+# brb
+#  ans is 17/42
+# x prob that 1 is red = 1/7, 1 is black = 1/7
+# y prob that 1 is red = 1/9, 1 is black = 1/9
+# z prob that 1 is red = 1/8, 1 is black = 1/8
+# compound event prob = 17 /42
