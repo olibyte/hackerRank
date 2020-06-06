a887cbe3e9ff070e8bff38a2aabf7b3483e92db6
Oliver <ocben1@gmail.com>
2020-06-06 21:30:17 +1000

    probability challenge

---
+++ 10_days_of_statistics/probability.py
@@ -0,0 +1,39 @@
+# 2 evenly weighted die (fair)
+# what is the probability that their sum will be at most 9?
+
+# BRUTE FORCE
+import math
+
+def get_outcomes_totals(arr1,arr2):
+    outcomes = []
+    totals = []
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            outcomes.append((arr1[i],arr2[j]))
+            totals.append(arr1[i] + arr2[j])
+    return outcomes, totals
+def get_prob(arr, val):
+    count = 0
+    n = len(arr)
+    for i in range(n):
+        if arr[i] <= val:
+            count += 1
+    return count / n
+def get_prob_more_dice(outcomes, totals, val):
+    count = 0
+    n = len(totals)
+    for i in range(n):
+        if totals[i] == val and outcomes[i][0] != outcomes[i][1]:
+            count +=1
+    return count / n
+def compound_prob():
+    return
+D1 = [1,2,3,4,5,6]
+D2 = [1,2,3,4,5,6]
+o, t = get_outcomes_totals(D1,D2)
+p = get_prob(t,9)
+p2 = get_prob_more_dice(o,t,6)
+# print(o)
+# print(t)
+# print(p)
+print(p2)
