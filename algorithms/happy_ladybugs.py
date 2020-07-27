2b0d01c7c664cb50daf384980d556911f44f2e47
Oliver <ocben1@gmail.com>
2020-07-28 08:36:48 +1000

    happy lady bugs

--- algorithms/happy_ladybugs.py
+++ algorithms/happy_ladybugs.py
@@ -8,8 +8,32 @@ import sys
 
 # Complete the happyLadybugs function below.
 def happyLadybugs(b):
-    return Null
-
+    if b.count("_")>0:
+        if b.count("_")==len(b):
+            return "YES"
+        else:
+            letters = list(set(list(b)))
+            letters.pop(letters.index("_"))
+            for i in letters:
+                if b.count(i)==1:
+                    return "NO"
+            return "YES"
+    else:
+        if len(b)==1:
+            return "NO"
+        else:
+            counter = 0
+            for i in range(len(b)):
+                if i==len(b)-1:
+                    if b[i]==b[i-1]:
+                        counter = counter+1
+                else:
+                    if b[i]==b[i+1] or b[i]==b[i-1]:
+                        counter = counter + 1
+            if counter==len(b):
+                return "YES"
+            else:
+                return "NO"
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
