9163b47f421d54e34ab3dd78e01825ea038b308c
Oliver <ocben1@gmail.com>
2020-07-29 19:43:22 +1000

    insertion sort 1

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the insertionSort1 function below.
+def insertionSort1(n, arr):
+    
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
