a426c63fb0df8c02e1d829e15044167d68c8a19a
Oliver <ocben1@gmail.com>
2020-07-31 06:43:57 +1000

    insertion sort inserting

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -8,7 +8,20 @@ import sys
 
 # Complete the insertionSort1 function below.
 def insertionSort1(n, arr):
-    # insertion sort algorithm goes here
+    tmp = arr[n-1]
+    for i in range(n-1,0,-1):
+        if tmp < arr[i-1]:
+            arr[i]=arr[i-1]
+        elif tmp > arr[i-1]:
+            arr[i]=tmp
+            break;
+        for i in arr:
+            print(i,end=" ")
+        print(" ")
+    if tmp < arr[0]:
+        arr[0]=tmp;
+    for i in (arr):
+        print(i, end= " ")
 if __name__ == '__main__':
     n = int(input())
 
