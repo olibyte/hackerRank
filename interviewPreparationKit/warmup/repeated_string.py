0909141967d52ff28238a5a3c59644608439b03b
Oliver Bennett <oliver.bennett@education.vic.gov.au>
2023-11-22 08:21:47 +1100

    simplified solution sans collections module

--- interviewPreparationKit/warmup/repeated_string.py
+++ interviewPreparationKit/warmup/repeated_string.py
@@ -5,43 +5,27 @@ import os
 import random
 import re
 import sys
-from collections import Counter
 
-def getFrequencyA(my_string):
-    count = dict(Counter(my_string))
-
-    keys = count.keys()
-
-    if 'a' in keys:
-        answer = count['a']
-    else:
-        answer = 0
-    return answer
 # Complete the repeatedString function below.
 def repeatedString(s, n):
-    if len(s) == 1 and s != 'a':
-        answer = 0
-    elif len(s) == 1 and s == 'a':
-        answer = n
-    elif len(s) > n:
-        new_string = list(s[:n])
-        
-        answer = getFrequencyA(new_string)
-    else:
-        loop_num = n // len(s)
-        mod_num = n % len(s)
+    # edge cases, if s is only 1 character long.
+    s_length = len(s)
+    if s_length == 1:
+        if s == 'a':
+            return n
+        else:
+            return 0
 
-        new_string = ''
+    frequency = 0
+    multiplier = n // s_length
+    remainder = n % s_length
+    
+    s_frequency = s.count('a')
+    remainder_frequency = s[:remainder].count('a')
+    frequency = (s_frequency * multiplier) + remainder_frequency
+     
+    return frequency
 
-        for i in range(loop_num):
-            new_string = new_string + s
-        new_string = new_string + s[:mod_num]
-
-        new_string = list(new_string)
-
-        answer = getFrequencyA(new_string)
-
-    return answer
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
